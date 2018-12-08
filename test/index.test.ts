34a572c6c0fcd1e1d7b687bf533cf0ff6ca52cd0
Andie <zchangan@163.com>
2018-12-08 22:59:26 +0800

    :white_check_mark: Share test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,59 +1,49 @@
 import assert = require('assert');
+import * as fs from 'fs';
 import Kevast = require('kevast');
 import * as tmp from 'tmp';
 import KevastFile = require('../index');
+import {basicTest} from './share';
 
 describe('Test basic function', () => {
-  let kevast: Kevast;
-  let tempFile: tmp.SynchrounousResult;
-  before(() => {
-    tempFile = tmp.fileSync();
-    kevast = new Kevast(new KevastFile(tempFile.name));
+  it('Path not exist', async () => {
+    const name = tmp.tmpNameSync();
+    this.kevast = new Kevast(new KevastFile(name));
+    await basicTest.call(this);
   });
-
-  it('Get', async () => {
-    assert(await kevast.get('key1') === null);
-    assert(await kevast.get('key1', 'default') === 'default');
+  it('Directory path given', async () => {
+    const dir = tmp.dirSync();
+    assert.throws(() => {
+      const kevast = new Kevast(new KevastFile(dir.name));
+    });
+    dir.removeCallback();
   });
-  it('Set', async () => {
-    await kevast.set('key1', 'value1');
-    assert(await kevast.get('key1') === 'value1');
+  it('File with invalid content given', () => {
+    const name = tmp.tmpNameSync();
+    fs.writeFileSync(name, '#$%%&^%^&');
+    assert.throws(() => {
+      const kevast = new Kevast(new KevastFile(name));
+    });
+    fs.unlinkSync(name);
   });
-  it('Has', async () => {
-    assert(await kevast.has('key1') === true);
-    assert(await kevast.has('key2') === false);
+  it('File path given', async () => {
+    const tempFile = tmp.fileSync();
+    this.kevast = new Kevast(new KevastFile(tempFile.name));
+    await basicTest.call(this);
+    tempFile.removeCallback();
   });
-  it('Size', async () => {
-    await kevast.set('key2', 'value2');
-    await kevast.set('key3', 'value3');
-    await kevast.set('key4', 'value4');
-    assert(await kevast.size() === 4);
+  it('File path buffer given', async () => {
+    const tempFile = tmp.fileSync();
+    const buffer = Buffer.from(tempFile.name);
+    this.kevast = new Kevast(new KevastFile(buffer));
+    await basicTest.call(this);
+    tempFile.removeCallback();
   });
-  it('Delete', async () => {
-    assert(await kevast.has('key4') === true);
-    await kevast.delete('key4');
-    assert(await kevast.has('key4') === false);
-  });
-  it('Entries', async () => {
-    const source = [...await kevast.entries()];
-    const target = [['key1', 'value1'], ['key2', 'value2'], ['key3', 'value3']];
-    assert.deepStrictEqual(source, target);
-  });
-  it('Keys', async () => {
-    const source = [...await kevast.keys()];
-    const target = ['key1', 'key2', 'key3'];
-    assert.deepStrictEqual(source, target);
-  });
-  it('Values', async () => {
-    const source = [...await kevast.values()];
-    const target = ['value1', 'value2', 'value3'];
-    assert.deepStrictEqual(source, target);
-  });
-  it('Clear', async () => {
-    await kevast.clear();
-    assert(await kevast.size() === 0);
-  });
-  after(() => {
+  it('File url given', async () => {
+    const tempFile = tmp.fileSync();
+    const url = new URL(`file://${tempFile.name}`);
+    this.kevast = new Kevast(new KevastFile(url));
+    await basicTest.call(this);
     tempFile.removeCallback();
   });
 });
